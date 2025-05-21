a70ccbb230e0b91a39d03f615fa1df241b8cb19d
Ashley Laisure <ashley.laisure@gmail.com>
2025-05-21 15:15:21 -0400

    model added

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
     # we are going to define all app-level urls in this list
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('cats/', views.cat_index, name="cat-index"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# Import HttpResponse to send text-based responses
+# from django.http import HttpResponse
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
 
 # Create your views here.
+# Define the home view function
 def home(request):
-    return HttpResponse('<h1>Hello World! ᓚᘏᗢ</h1>')
+    # Send a simple HTML response
+    return render(request, 'home.html')
 
 def about(request):
     # return HttpResponse('<h1>About the Cat Collector</h1>')
     return render(request, 'about.html')
+
+def cat_index(request):
+    # Render the cats/index.html template with the cats data
+    return render(request, 'cats/index.html', {'cats' : cats})
