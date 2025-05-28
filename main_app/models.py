51fc4d68b75451a2826f750bf1b129afecce09ad
Ashley Laisure <ashley.laisure@gmail.com>
2025-05-27 20:38:00 -0400

    One-to-many relationship cat-feeding added

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -9,3 +15,19 @@ class Cat(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("cat-detail", kwargs={"cat_id": self.id})
+
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+    
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.get_meal_display()} on {self.date}'
+    
+    class Meta:
+        # This line makes the newest feedings appear first
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Import HttpResponse to send text-based responses
 # from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
+from .forms import FeedingForm
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
 # Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+# cats = [
+#     Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
 
 # Create your views here.
 # Define the home view function
@@ -29,4 +33,42 @@ def about(request):
 
 def cat_index(request):
     # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats' : cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat' : cat,
+        'feeding_form' : feeding_form
+        })
+
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
+
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    # can list the fields availiable to the user
+    # fields = ['name', 'breed', 'description', 'age']
+    # success_url = '/cats/' redirect to index
+    # Remove success_url so Django uses get_absolute_url from Cat
+    
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+    
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
