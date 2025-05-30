010fa1c336f8542bcb1fea43d1e7fdbc06e06609
Ashley Laisure <ashley.laisure@gmail.com>
2025-05-29 20:07:33 -0400

    Many to many added

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 # Import HttpResponse to send text-based responses
 # from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Cat
+from django.views.generic import ListView, DetailView
+from .models import Cat, Toy
 from .forms import FeedingForm
 
 
@@ -38,10 +39,15 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
+    toys = Toy.objects.all()  # Fetch all toys
+    # Only get the toys the cat does not have
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
+
     feeding_form = FeedingForm()
     return render(request, 'cats/detail.html', {
         'cat' : cat,
-        'feeding_form' : feeding_form
+        'feeding_form' : feeding_form,
+        'toys' : toys_cat_doesnt_have,
         })
 
 def add_feeding(request, cat_id):
@@ -59,9 +65,9 @@ def add_feeding(request, cat_id):
 
 class CatCreate(CreateView):
     model = Cat
-    fields = '__all__'
+    # fields = '__all__'
     # can list the fields availiable to the user
-    # fields = ['name', 'breed', 'description', 'age']
+    fields = ['name', 'breed', 'description', 'age']
     # success_url = '/cats/' redirect to index
     # Remove success_url so Django uses get_absolute_url from Cat
     
@@ -72,3 +78,33 @@ class CatUpdate(UpdateView):
 class CatDelete(DeleteView):
     model = Cat
     success_url = '/cats/'
+    
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+    
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+def associate_toy(request, cat_id, toy_id):
+    # Note that you can pass a toy's id instead of the whole object
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('cat-detail', cat_id=cat_id)
+
+def remove_toy(request, cat_id, toy_id):
+    # Look up the cat
+    # Look up the toy
+    # Remove the toy from the cat
+    Cat.objects.get(id=cat_id).toys.remove(toy_id)
+    return redirect('cat-detail', cat_id=cat_id)
